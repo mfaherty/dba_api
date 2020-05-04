ad3f7c29bcd0b8e627acd52850a376ffb5b8a1f6
mfaherty <mary.faherty@me.com>
2020-05-04 20:10:23 +0100

    Adding Functions
    Broke script into functions which can be tested later.

--- api.py
+++ api.py
@@ -5,98 +5,59 @@ Created on Thu Apr 30 14:55:35 2020
 @author: maryf
 """
 
+
+# Importing libraries nedded.
+
 from bs4 import BeautifulSoup
 import requests
 import csv
+import unittest 
+
+#url to scrap
 
 url= 'https://www.worldometers.info/coronavirus/countries-where-coronavirus-has-spread/'
 
+#Function to run requests.get. and get data from url. use BS4 to return html data 
 
-
-r = requests.get(url).text
+def getdata(url):
+    r = requests.get(url).text
 
 #print(r)
-
-soup = BeautifulSoup(r,'html.parser')
+    soup = BeautifulSoup(r,'html.parser')
+    return soup
 
 #print(soup)
 
-apitable = soup.find_all("tr")
+covid_table = soup.find_all("tr")
+print(covid_table)
 
-output_rows = []
-for table_row in soup.findAll('tr'):
-    columns = table_row.findAll('td')
-    output_row = []
-    for column in columns:
-        output_row.append(column.text)
-    output_rows.append(output_row)
-    #output_rows = [x.strip(' ') for x in output_rows]
-    #final=list(map(lambda x:x.strip(),output_rows))
-    #print(final)
+# Function to search through response r for data in the table.
+
+def search_data():
+    output_rows = []
+    for table_row in soup.findAll('tr'):
+        columns = table_row.findAll('td')
+        output_row = []
+        for column in columns:
+            output_row.append(column.text)
+        output_rows.append(output_row)
+        #output_rows = [x.strip(' ') for x in output_rows]
+        #final=list(map(lambda x:x.strip(),output_rows))
+        #print(final)
+        return output_rows
     
-#print(output_rows)
+print(output_rows)
 #print(type(final))
     
-with open('output.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["Country", "Cases", "Deaths", "Region"]) 
-    writer.writerows(output_rows)                            
-                              
-                              
-                              
-                              
-                              
-'''
-allapis = soup.find_all("tr")
-l={}
-u=list()
-
-
-
+# function to write the date to an output file. 
+def write_function():
     
-
-for i in range(0,len(allapis)):
-                    #try:
-                    api = allapis[i].find_all("td")
-                    #except:
-                        #api=None
+    with open('output.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Country", "Cases", "Deaths", "Region"]) 
+        writer.writerows(output_rows)                            
+                              
+                              
+                              
+                              
                     
-                    try:
-                        l["Country"]=api[0].text.replace("\n","")
-                    except:
-                        l["api"]=None
-                        
-                    try:
-                        l["Cases"]=api[1].text.replace("\n","")
-                    except:
-                        l["api"]=None
-                        
-                    try:
-                        l["Deaths"]=api[2].text.replace("\n","")
-                    except:
-                        l["api"]=None
-                        
-
-                    try:
-                        l["Region"]=api[2].text.replace("\n","")
-                    except:
-                        l["api"]=None
-                    
-                    
-                        
-                    u.append(l)
-                    l={}
-                    
-                   
-
-## then we open a csv file in append mode
-with open("stats.csv", "a") as csv_file:
-    writer = csv.writer(csv_file)  
-    writer.writerow(["Country", "Cases", "Deaths", "Region"])      
-
-print(u)
-print(type(u))
-print(l)
-print(type(l))
-                        
-'''                        
